01fc0f52b64de84aec3106a641b053d4101b2295
Berke Akbay <62380801+BioKZM@users.noreply.github.com>
2021-12-16 18:30:27 +0300

    Update classes.py

--- functions/classes.py
+++ functions/classes.py
@@ -6,7 +6,7 @@ from discord.utils import get
 
 client = commands.Bot(command_prefix=['!','-'], intents=discord.Intents.all(),help_command=None,case_insensitive=True)
 
-serverURL ="https://colonist-test-default-rtdb.firebaseio.com/"
+serverURL =os.environ['TOKEN']
 
 experiences = [99,699,1987,6666,9999,30000,90000,300000,900000,10000001]
 levelNames = ["Unit","Guest","Colony Member","Open Crew","Crew","Captain","Judge","Colony Manager","Mars Lover","Chief of the Colony","Partner"] 
