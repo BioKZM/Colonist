5cbc7222299def3f53d5311e81dee6503a775830
BioKZM <qRnt@yandex.com>
2021-12-19 15:42:11 +0300

    New UI System

--- main.py
+++ main.py
@@ -77,6 +77,8 @@ async def yardım(ctx):
 	embed.add_field(name="!yardım `[ !help, !komutlar ]`",value="Bot üzerinde bulunan mevcut komutları görüntülemenizi sağlar.",inline=False)
 	embed.add_field(name="!seviye `[ !level ]`",value="Mevcut ve gelecek seviye değerlerini gösteririr.",inline=False)
 	embed.add_field(name="!sıralama `[ !rank ]`",value="Güncel liderlik tablosunu gösterir.",inline=False)
+	embed.add_field(name="!bilgi",value="Sunucu hakkında detaylı bilgi almanı sağlar.",inline=False)
+	embed.add_field(name="!mesajsil",value="En son attığın bilgi mesajını silmeni sağlar.",inline=False)
 	await ctx.channel.send(embed=embed)
 
 
